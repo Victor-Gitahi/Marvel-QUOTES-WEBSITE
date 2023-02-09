d2d9044a6a27357ea434554844c96e33e7a233f4
Victor-Gitahi <gitahi14043@student.mmarau.ac.ke>
2023-02-09 21:26:23 +0300

    File and folder set up

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/marvel', require('./routes/marvelRoutes'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
